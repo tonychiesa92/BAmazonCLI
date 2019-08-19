b9b8f431f80c954d86b5b982ff7fb986bbd20af1
Tony C <tonychiesa92@gmail.com>
2019-08-19 02:49:34 -0400

    created data base with table and put mock data in

---
+++ bamazon.sql
@@ -0,0 +1,46 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products(
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(50) NOT NULL,
+  department_name VARCHAR(50) NOT NULL,
+  price DECIMAL(10,3)NULL,
+  stock_quantity INT,
+  PRIMARY KEY (item_id)
+);
+
+SELECT * FROM products;
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("White T-Shirt", "Clothing Department", 9.99, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Running Shoes", "Shoe Department", 57.89, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Tap Dancing Shoes", "Shoe Department", 42.29, 5);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Blue Dress", "Clothing Department", 124.55, 7);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bananas", "Grocery Store", 0.66, 7999);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Laptop", "Electronics", 999.99, 15);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Ipod", "Electronics", 99.99, 2);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("", "", 9.99, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Steak", "Grocery Store", 9.99, 44);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bluetooth Speaker", "Electronics", 39.79, 6);
