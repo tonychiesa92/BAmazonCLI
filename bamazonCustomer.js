da40d23bce7d044ebaf132187721f7b5c9c345c9
Tony C <tonychiesa92@gmail.com>
2019-08-22 05:39:07 -0400

    reinstalled node modules; prompt is partially working but not re-displaying correctly

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -25,7 +25,7 @@ connection.connect(function (err) {
     // Logs the connection.threadId
     console.log("connected as id " + connection.threadId);
 
-    displayBamazonDB();
+   
 
 });
 
@@ -33,41 +33,59 @@ function displayBamazonDB() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
 
-        var displayTable = new Table ({
-			head: ["Item ID", "Product Name", "Catergory", "Price", "Quantity"],
-			colWidths: [10,25,25,10,14]
-		});
-		for(var i = 0; i < res.length; i++){
-			displayTable.push(
-				[res[i].item_id,res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
-				);
-		}
-		console.log(displayTable.toString());
-
-        
-        startShopping();
-    });
-}
-
-function startShopping() {
-    inquirer
-        .prompt([
-            {
-                name: "item",
-                type: "input",
-                message: "What item ID do you want to purchase?"
-            },
-            {
-                name: "amount",
-                type: "input",
-                message: "How many units of the product would you like to purchase?"
-            }])
-        .then(function (answer) {
-
-
-            // Logs answers
-            // console.log(answer);
-
-            connection.end();
+        var displayTable = new Table({
+            head: ["Item ID", "Product Name", "Catergory", "Price"],
+            colWidths: [10, 25, 25, 10]
         });
-}
+        for (var i = 0; i < res.length; i++) {
+            displayTable.push(
+                [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price]
+            );
+        }
+        console.log(displayTable.toString());
+
+
+        purchasePrompt();
+    });
+};
+
+function purchasePrompt() {
+    inquirer.prompt([
+        {
+            name: "ID",
+            type: "input",
+            message: "Please enter Item ID you like to purhcase.",
+            filter: Number
+        },
+        {
+            name: "Quantity",
+            type: "input",
+            message: "How many items do you wish to purchase?",
+            filter: Number
+        },
+        ]).then(function (answers) {
+            var quantityNeeded = answers.Quantity;
+            var IDrequested = answers.ID;
+            purchaseOrder(IDrequested, quantityNeeded);
+        });
+};
+
+function purchaseOrder(ID, amtNeeded) {
+
+    connection.query('SELECT * FROM products WHERE item_id = ' + ID, function (err, res) {
+        if (err) throw err;
+
+        if (amtNeeded <= res[0].stock_quantity) {
+            var totalCost = res[0].price * amtNeeded;
+            console.log("Good news your order is in stock!");
+            console.log("Your total cost for " + amtNeeded + " " + res[0].product_name + " is " + totalCost + " Thank you!");
+
+            connection.query("UPDATE products SET stock_quantity = stock_quantity - " + amtNeeded + "WHERE item_id = " + ID);
+        } else {
+            console.log("Insufficient quantity, sorry we do not have enough " + res[0].product_name + " to complete your order.");
+        };
+        displayBamazonDB();
+    });
+};
+
+displayBamazonDB();
